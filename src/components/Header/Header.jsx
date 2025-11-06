7719332d107a6e01e6b53a0af98e7587683b537d
Shikhar Pandey <pandeyshikhar007@gmail.com>
2025-11-06 12:01:18 +0530

    adding theme changing button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,56 @@
-import React from 'react'
-import {Link,NavLink} from "react-router-dom";
-import './Header.css';
-
-
+import React, { useState, useRef, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setShowDropdown((prev) => !prev);
+  };
+
+  // Close dropdown on outside click
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setShowDropdown(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <header>
-       <nav>
+      <nav>
         <div id="logo">
           <Link to="/">ShikharPandey</Link>
         </div>
+
         <div id="content">
           <ul>
-            <li>
-              <NavLink to="/">Dashboard</NavLink>
-            </li>
-            <li>
-              <NavLink to="/tasks">Tasks</NavLink>
-            </li>
-            <li>
-              <NavLink to="/members">Members</NavLink>
-            </li>
-            <li>
-              <NavLink to="/settings">Settings</NavLink>
+            <li><NavLink to="/">Dashboard</NavLink></li>
+            <li><NavLink to="/tasks">Tasks</NavLink></li>
+            <li><NavLink to="/members">Members</NavLink></li>
+
+            {/* Settings Dropdown */}
+            <li className="dropdown" ref={dropdownRef}>
+              <button className="dropdown-btn" onClick={toggleDropdown}>
+                Settings
+              </button>
+
+              {showDropdown && (
+                <div className="dropdown-menu">
+                  <button className="theme-btn">Change Theme</button>
+                </div>
+              )}
             </li>
           </ul>
         </div>
       </nav>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Settings() {
   return (
     <div>Settings</div>
+    
   )
 }
 
